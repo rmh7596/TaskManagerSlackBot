6e3403eac0a6b1d456a655d75bc2b472bdc89ce8
Ryan Haver <rmh7596@rit.edu>
2021-12-17 13:03:20 -0500

    Cleanup

--- database.py
+++ database.py
@@ -1,47 +1,49 @@
 import sqlite3
 import status
 
-def store(fileName, userID, taskName, dueDate, ts):
-    connection = sqlite3.connect(fileName)
-    cursor = connection.cursor()
+def store(fileName, userID, taskName, dueDate, ts, channel_id, link):
+    connection     = sqlite3.connect(fileName)
+    cursor         = connection.cursor()
     cursor.execute("SELECT * FROM Tasks WHERE timestamp=?", (ts,))
-    possibleTask = cursor.fetchall()
+    possibleTask   = cursor.fetchall()
+
     if len(possibleTask) == 0:
-        connection.execute("INSERT INTO Tasks VALUES (?,?,?,?)", (userID, taskName, dueDate, ts))
+        connection.execute("INSERT INTO Tasks VALUES (?,?,?,?,?,?)", (userID, taskName, dueDate, ts, channel_id, link))
         connection.commit()
         connection.close()
         return status.databaseStatus.STORED
+
     elif len(possibleTask) > 0:
         # Update
         connection.execute("UPDATE Tasks SET dueDate=(?) WHERE taskName=(?)", (dueDate, taskName))
         connection.commit()
         connection.close()
         return status.databaseStatus.UPDATED
+
     else:
         return status.databaseStatus.ERROR
 
 def getTasks(fileName, userID):
     connection = sqlite3.connect(fileName)
-    cursor = connection.cursor()
+    cursor     = connection.cursor()
     cursor.execute("SELECT * FROM Tasks WHERE userID=?", (userID,))
-    taskData = cursor.fetchall()
+    taskData   = cursor.fetchall()
     connection.close()
     return taskData
 
 def getTasksByDate(fileName, date):
     connection = sqlite3.connect(fileName)
-    cursor = connection.cursor()
+    cursor     = connection.cursor()
     cursor.execute("SELECT * FROM Tasks WHERE dueDate=?", (date,))
-    taskData = cursor.fetchall()
+    taskData   = cursor.fetchall()
     connection.close()
     return taskData
 
 def remove(fileName, ts):
     connection = sqlite3.connect(fileName)
-    cursor = connection.cursor()
+    cursor     = connection.cursor()
     cursor.execute("DELETE FROM Tasks WHERE timestamp=?", (ts,))
     connection.commit()
     connection.close()
-
     return status.databaseStatus.REMOVED
 
--- main.py
+++ main.py
@@ -24,8 +24,10 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 @app.route('/create-task', methods=['POST'])
 def create_task():
-    data = request.form
-    input = data.get('text')
+    log.info("task created")
+    data       = request.form
+    log.debug(data)
+    input      = data.get('text')
     channel_id = data.get('channel_id')
 
     if input == "":
@@ -33,22 +35,24 @@ def create_task():
         return Response(), 200
     
     taskName = input
-    view = taskCreationView.Task(taskName)
+    view     = taskCreationView.Task(taskName)
     client.chat_postMessage(channel=channel_id, **view.getMessage())
     return Response(), 200
 
 @app.route('/submit-deadline', methods=['POST'])
 def get_deadline():
     log.info("deadline submitted")
-    rawData = request.form
-    parsedData = json.loads(rawData["payload"])
+    rawData      = request.form
+    parsedData   = json.loads(rawData["payload"])
     log.info(f"parsed data {parsedData}", parsedData)
     selectedDate = parsedData['state']['values']['dateSelectionBox']['datePicker']['selected_date']
-    task = parsedData['message']['blocks'][0]['text']['text'][16:] # Substringing to remove the "*Task Created: xxx *", need a better way
-    userID = parsedData['user']['id']
-    ts = parsedData['container']['message_ts']
+    task         = parsedData['message']['blocks'][0]['text']['text'][16:] # Substringing to remove the "*Task Created: xxx *"
+    userID       = parsedData['user']['id']
+    ts           = parsedData['container']['message_ts']
+    channel_id   = parsedData['container']['channel_id']
+    link         = client.chat_getPermalink(channel=channel_id, message_ts=ts).get('permalink')
 
-    if database.store(os.environ['DATABASE'], userID, task, selectedDate, ts) == status.databaseStatus.ERROR:
+    if database.store(os.environ['DATABASE'], userID, task, selectedDate, ts, channel_id, link) == status.databaseStatus.ERROR:
         log.error("Unable to update database entry")
         raise errors.databaseStorageError
 
@@ -58,26 +62,35 @@ def get_deadline():
 def reactionAdded(payload):
     log.debug("reaction added")
     log.info(f"payload: {payload}", payload)
-    event = payload.get('event', {})
-    channel_id = event.get('item', {}).get('channel')
-    current_timestamp = event.get('item', {}).get('ts')
-    if event.get('reaction') == 'white_check_mark':
-        database.remove(os.environ['DATABASE'], current_timestamp)
-        client.chat_delete(channel=channel_id, ts=current_timestamp)
+    event             = payload.get('event', {})
+    channel_id        = event.get('item', {}).get('channel')
+    message_timestamp = event.get('item', {}).get('ts')
 
+    if event.get('reaction') == 'white_check_mark':
+        log.debug("Removing task")
+        database.remove(os.environ['DATABASE'], message_timestamp)
+        client.chat_delete(channel=channel_id, ts=message_timestamp)
+
+    if event.get('reaction') == '+1':
+        log.debug("Removing reminder")
+        client.chat_delete(channel=channel_id, ts=message_timestamp)
 
 scheduler = BackgroundScheduler()
-@scheduler.scheduled_job(IntervalTrigger(days=1), max_instances=1)
+@scheduler.scheduled_job(IntervalTrigger(days=1))
 def reminders():
     today_tasks = database.getTasksByDate(os.environ['DATABASE'], date.today())
+    log.info(f"Todays reminder to send out: {today_tasks}", today_tasks)
     for task in today_tasks:
-        user_id = task[0]
+        user_id   = task[0]
         task_name = task[1]
-        user_str = "@" + user_id
-        client.chat_postMessage(channel=user_str, text="*Reminder*: Your task: " + task_name + " is due today")
+        user_str  = "@" + user_id
+        link      = task[5]
+
+        client.chat_postMessage(channel=user_str, text="*Reminder*: " + task_name + " is due today. Clear reminder using :+1:. Clear task <" + link + "|here>")
 
 
 if __name__ == "__main__":
-    scheduler.start()
     log.basicConfig(filename=os.environ['LOGGING'], encoding='utf-8', level=log.DEBUG)
+    log.info("Starting scheduler")
+    scheduler.start()
     app.run(debug=True, use_reloader=False)
